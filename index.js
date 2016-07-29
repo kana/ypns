e3bb9d52b5775afa34a5767fe8af998a1ee6274f
Kana Natsuno <dev@whileimautomaton.net>
2016-07-29 22:25:02 +0900

    Redefine signIn with promise

--- index.js
+++ index.js
@@ -12,66 +12,22 @@ fs.appendFileSync(cookiePath, '', {mode: 0o600})
 const jar = _request.jar(new FileCookieStore(cookiePath))
 const request = _request.defaults({jar: jar, followAllRedirects: true})
 
-function defaultOnError(error) {
-  console.log('ERROR', error)
-}
-
-function visitTopPage() {
-  console.log('INFO', 'Opening the top page')
-  request('https://splatoon.nintendo.net/', function (error, response, body) {
-    if (error) {
-      console.log(error)
-      return
-    }
-
-    const $ = cheerio.load(body)
-    const loginButton = $('.nnid-login-btn')
-    if (loginButton.length) {
-      visitLoginPage()
-    } else {
-      console.log(body)
-      crawlOnlineFriendList($)
-    }
-  })
-}
-
-function visitLoginPage() {
-  console.log('INFO', 'Opening the log-in page')
-  request('https://splatoon.nintendo.net/users/auth/nintendo', function (error, response, body) {
-    if (error) {
-      console.log(error)
-      return
-    }
-
+function signIn() {
+  console.log('INFO', 'Signing in')
+  return request('https://splatoon.nintendo.net/users/auth/nintendo').then(function (body) {
     const $ = cheerio.load(body)
     const form = $('input[value="https://splatoon.nintendo.net/users/auth/nintendo/callback"]').closest('form')
-    if (form.length) {
-      tryLogin(form)
-    } else {
-      console.log('Error: Something wrong on log-in page')
-    }
-  })
-}
+    const map = {}
+    form.serializeArray().forEach(function (pair) {
+      map[pair.name] = pair.value
+    })
+    map.username = config.nnidUsername
+    map.password = config.nnidPassword
 
-function tryLogin(form) {
-  console.log('INFO', 'Logging in')
-  const map = {}
-  form.serializeArray().forEach(function (pair) {
-    map[pair.name] = pair.value
-  })
-  map.username = config.nnidUsername
-  map.password = config.nnidPassword
-
-  request.post({
-    url: form.attr('action'),
-    form: map
-  }, function (error, response, body) {
-    if (error) {
-      console.log(error)
-      return
-    }
-
-    crawlOnlineFriendList(cheerio.load(body))
+    return request.post({
+      url: form.attr('action'),
+      form: map
+    })
   })
 }
 
