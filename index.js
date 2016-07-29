6c9c02eb9b4854b8d3cc2dc5057bbf1894619e5c
Kana Natsuno <dev@whileimautomaton.net>
2016-07-29 20:28:16 +0900

    Open the top page with a custom cookie

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const FileCookieStore = require('tough-cookie-filestore')
+const _request = require('request')
+const cheerio = require('cheerio')
 const config = require('./config.json')
+const fs = require('fs')
 const slack = require('slack')
 
-slack.chat.postMessage({
-  token: config.token,
-  channel: config.channel,
-  username: config.username,
-  icon_emoji: config.icon_emoji,
-  text: 'XXX became online'
-}, function (err, data) {
-  console.log('err', err)
-  console.log('data', data)
+const cookiePath = 'cookie.json'
+
+// File given to FileCookieStore must exist.
+fs.appendFileSync(cookiePath, '', {mode: 0o600})
+const jar = _request.jar(new FileCookieStore(cookiePath))
+const request = _request.defaults({jar: jar})
+
+request('https://splatoon.nintendo.net/', function (error, response, body) {
+  if (error) {
+    console.log(error)
+    return
+  }
+
+  const $ = cheerio.load(body)
+  const loginButton = $('.nnid-login-btn')
+  console.log(loginButton.length)
+  console.log(loginButton.attr('href'))
 })
